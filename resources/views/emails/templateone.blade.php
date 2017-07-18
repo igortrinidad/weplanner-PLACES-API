99b41e8aaa7fa1275fba39cf44400482c3182a44
Andre <andrebf4@gmail.com>
2017-07-18 13:05:53 -0300

    template email

--- resources/views/emails/templateone.blade.php
+++ resources/views/emails/templateone.blade.php
@@ -1,113 +1,148 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8">
-  <title>WePlaces - Informativo</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://fonts.googleapis.com/css?family=Quicksand:400,500" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
+    <meta charset="utf-8">
+    <title>WePlaces - Informativo</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Quicksand:400,500" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
+    <style media="screen">
+        * { padding: 0; margin: 0; box-sizing: border-box; }
+    </style>
 </head>
-<style>
-  * { margin: 0; padding: 0; box-sizing: border-box; }
-  html, body {
-    font-family: 'Lato', sans-serif;
-    background-color: #f2f2f2;
-    color: #2c3e50;
-  }
-  header {
-    background-color: #7bccc6;
-    padding: 20px 0;
-    text-align: center;
-  }
-  a {
-    color: #7bccc6;
-    text-decoration: none;
-  }
-</style>
-<body>
 
-  <header>
-    <h1>
-      <img
-        src="https://blog.weplaces.com.br/wp-content/themes/blog-weplanner/images/logo.png"
-        width="200px"
-        alt="We Places"
-      >
-    </h1>
-  </header>
+<body style="font-family: 'Lato', sans-serif; background-color: #fff; color: #2c3e50;">
 
-  <main>
-    <div style="max-width: 768px; margin: 0 auto; padding: 0 10px;">
-      <div style="margin: 0 -10px; clear: both;">
-
-        <div style="padding: 10px; width: 100%; display:block; float: left;">
-          <div style="background: #fff; width: 100%; padding: 10px; text-align: center">
-            <h2 style="font-family: 'Quicksand'; font-weight: 200;">Visualizações do mês</h2>
-            <span style="font-size: 20px;"><strong>1243</strong></span>
-          </div>
+    <header style="text-align: center; padding-bottom: 30px;">
+        <div style="max-width: 700px; margin: 0 auto; padding: 0 10px;">
+            <div style="clear: both;">
+                <img
+                    width="100%"
+                    src="https://blog.weplaces.com.br/wp-content/uploads/2017/07/Header_Email_Weekly.png"
+                    alt="We Places Cabeçalho"
+                />
+            </div>
+            <div style="width: 100%; display:block;">
+                <h1 style="font-family: 'Quicksand', sans-serif; font-weight: 400;">Torre de Cristal</h1>
+                <span>18.Jun.2017</span>
+            </div>
         </div>
+    </header>
 
-        <div style="padding: 10px; width: 50%; display:block; float: left;">
-          <div style="background: #fff; width: 100%; padding: 10px;">
-            <h2 style="font-family: 'Quicksand'; font-weight: 200;">Tempo</h2>
-            <span style="font-size: 20px;"><strong>1243</strong></span>
-          </div>
+
+    <main>
+        <div style="max-width: 700px; margin: 0 auto; padding: 30px 10px;">
+            <div style="clear: both;">
+                <div style="width: 50%; display:block; float: left;  text-align: center;">
+                    <div style="border-right: 1px solid #f2f2f2; border-bottom: 1px solid #f2f2f2; width: 100%; padding: 25px; text-align: center;">
+                        <h2 style="font-family: 'Quicksand', sans-serif; font-weight: 400;">Visualizações do mês</h2>
+                        <span style="font-size: 20px;"><strong>1243</strong></span>
+                        <br />
+                        <span style="margin-top: 10px; text-transform: uppercase; font-weight: 700; font-family: 'Quicksand', sans-serif; font-size: 12px;">total: 6726</span>
+                    </div>
+                </div>
+
+                <div style="width: 50%; display:block; float: left;  text-align: center;">
+                    <div style="border-left: 1px solid #f2f2f2; border-bottom: 1px solid #f2f2f2; width: 100%; padding: 25px;">
+                        <h2 style="font-family: 'Quicksand', sans-serif; font-weight: 400;">Tempo de Permanência</h2>
+                        <span style="font-size: 20px;"><strong>1243:<small style="font-size: 12px;">horas</small></strong></span>
+                        <br />
+                        <span style="margin-top: 10px; text-transform: uppercase; font-weight: 700; font-family: 'Quicksand', sans-serif; font-size: 12px;">total: 6726</span>
+                    </div>
+                </div>
+
+                <div style="width: 50%; display:block; float: left;  text-align: center;">
+                    <div style="border-right: 1px solid #f2f2f2; border-top: 1px solid #f2f2f2; width: 100%; padding: 25px;">
+                        <h2 style="font-family: 'Quicksand', sans-serif; font-weight: 400;">Clicks no contato <small style="font-size: 12px;">(Whatsapp)</small></h2>
+                        <span style="font-size: 20px;"><strong>1243</strong></span>
+                        <br />
+                        <span style="margin-top: 10px; text-transform: uppercase; font-weight: 700; font-family: 'Quicksand', sans-serif; font-size: 12px;">total: 6726</span>
+                    </div>
+                </div>
+
+                <div style="width: 50%; display:block; float: left;  text-align: center;">
+                    <div style="border-left: 1px solid #f2f2f2; border-top: 1px solid #f2f2f2; width: 100%; padding: 25px;">
+                        <h2 style="font-family: 'Quicksand', sans-serif; font-weight: 400;">Clicks no contato <small style="font-size: 12px;">(Mensagem)</small></h2>
+                        <span style="font-size: 20px;"><strong>1243</strong></span>
+                        <br />
+                        <span style="margin-top: 10px; text-transform: uppercase; font-weight: 700; font-family: 'Quicksand', sans-serif; font-size: 12px;">total: 6726</span>
+                    </div>
+                </div>
+
+                <!-- Compartilhamentos -->
+                <div style="width: 100%; margin-top: 30px; display:block; float: left;">
+                    <div style="width: 100%; padding: 10px;">
+                        <h2 style="font-family: 'Quicksand', sans-serif; font-weight: 700; text-transform: uppercase;">Compartilhamentos</h2>
+                    </div>
+                </div>
+
+                <div style="width: 80%; margin-top:5px; display:block; float: left;">
+                    <div style="background: #f2f2f2; border-radius: 5px 0px 0px 5px; overflow: hidden; height: 60px; width: 100%; padding: 23px;">
+                        <h2 style="font-family: 'Quicksand', sans-serif; font-weight: 700; text-transform:uppercase; font-size: 15px;">Compartilhamentos <small style="font-size: 12px;">Facebook</small></h2>
+                    </div>
+                </div>
+                <div style="width: 20%; margin-top:5px; display:block; float: left;">
+                    <div style="background: #f2f2f2; border-radius: 0px 5px 5px 0px; overflow: hidden; height: 60px; border-left: 2px solid #fff; width: 100%; padding: 17px;">
+                        <span style="font-size: 12px;"><strong>1243</strong></span>
+                        <br />
+                        <span style="margin-top: 10px; text-transform: uppercase; font-weight: 700; font-family: 'Quicksand', sans-serif; font-size: 10px;">
+                            total:
+                            <span style="color: #82CB7D">6726</span>
+                        </span>
+                    </div>
+                </div>
+
+                <div style="width: 80%; margin-top:5px; display:block; float: left;">
+                    <div style="background: #f2f2f2; border-radius: 5px 0px 0px 5px; overflow: hidden; height: 60px; width: 100%; padding: 23px;">
+                        <h2 style="font-family: 'Quicksand', sans-serif; font-weight: 700; text-transform:uppercase; font-size: 15px;">Compartilhamentos <small style="font-size: 12px;">Whatsapp</small></h2>
+                    </div>
+                </div>
+                <div style="width: 20%; margin-top:5px; display:block; float: left;">
+                    <div style="background: #f2f2f2; border-radius: 0px 5px 5px 0px; overflow: hidden; height: 60px; border-left: 2px solid #fff; width: 100%; padding: 17px;">
+                        <span style="font-size: 12px;"><strong>1243</strong></span>
+                        <br />
+                        <span style="margin-top: 10px; text-transform: uppercase; font-weight: 700; font-family: 'Quicksand', sans-serif; font-size: 10px;">
+                            total:
+                            <span style="color: #82CB7D">6726</span>
+                        </span>
+                    </div>
+                </div>
+
+                <div style="width: 80%; margin-top:5px; display:block; float: left;">
+                    <div style="background: #f2f2f2; border-radius: 5px 0px 0px 5px; overflow: hidden; height: 60px; width: 100%; padding: 23px;">
+                        <h2 style="font-family: 'Quicksand', sans-serif; font-weight: 700; text-transform:uppercase; font-size: 15px;">Compartilhamentos <small style="font-size: 12px;">Link</small></h2>
+                    </div>
+                </div>
+                <div style="width: 20%; margin-top:5px; display:block; float: left;">
+                    <div style="background: #f2f2f2; border-radius: 0px 5px 5px 0px; overflow: hidden; height: 60px; border-left: 2px solid #fff; width: 100%; padding: 17px;">
+                        <span style="font-size: 12px;"><strong>1243</strong></span>
+                        <br />
+                        <span style="margin-top: 10px; text-transform: uppercase; font-weight: 700; font-family: 'Quicksand', sans-serif; font-size: 10px;">
+                            total:
+                            <span style="color: #82CB7D">6726</span>
+                        </span>
+                    </div>
+                </div>
+                <!-- /Compartilhamentos  -->
+
+            </div>
         </div>
+    </main>
 
-        <div style="padding: 10px; width: 50%; display:block; float: left;">
-          <div style="background: #fff; width: 100%; padding: 10px;">
-            <h2 style="font-family: 'Quicksand'; font-weight: 200;">Clicks no contato <small>(Whatsapp)</small></h2>
-            <span style="font-size: 20px;"><strong>1243</strong></span>
-          </div>
+    <footer style="clear:both; padding: 30px 0 0 0; text-align: center;">
+        <div style="max-width: 700px; margin: 0 auto;">
+            <div style="margin: 0 -10px;">
+                <div style="padding: 0 10px;">
+                    <div style="background: #f2f2f2; border-radius: 5px; padding: 20px 0">
+                        <span style="font-size: 10px">
+                            Não quer receber mais e-mails como esse? <a href="#" style="color: #7bccc6; text-decoration: none;">Desinscreva-se aqui.</a>
+                        </span>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <div style="padding: 10px; width: 50%; display:block; float: left;">
-          <div style="background: #fff; width: 100%; padding: 10px;">
-            <h2 style="font-family: 'Quicksand'; font-weight: 200;">Clicks no contato <small>(Mensagem)</small></h2>
-            <span style="font-size: 20px;"><strong>1243</strong></span>
-          </div>
-        </div>
-
-        <div style="padding: 10px; width: 50%; display:block; float: left;">
-          <div style="background: #fff; width: 100%; padding: 10px;">
-            <h2 style="font-family: 'Quicksand'; font-weight: 200;">Compartilhamentos (Link)</h2>
-            <span style="font-size: 20px;"><strong>1243</strong></span>
-          </div>
-        </div>
-
-        <div style="padding: 10px; width: 50%; display:block; float: left;">
-          <div style="background: #fff; width: 100%; padding: 10px;">
-            <h2 style="font-family: 'Quicksand'; font-weight: 200;">Compartilhamentos (Facebook)</h2>
-            <span style="font-size: 20px;"><strong>1243</strong></span>
-          </div>
-        </div>
-
-        <div style="padding: 10px; width: 50%; display:block; float: left;">
-          <div style="background: #fff; width: 100%; padding: 10px;">
-            <h2 style="font-family: 'Quicksand'; font-weight: 200;">Compartilhamentos (Whatsapp)</h2>
-            <span style="font-size: 20px;"><strong>1243</strong></span>
-          </div>
-        </div>
-
-        <div style="padding: 10px; width: 100%; display:block; float: left;">
-          <img
-            src="https://lh3.googleusercontent.com/p1cxnnujscOVataXb_B57efe9t5FllCNFwjjt85WMtDhhgqmALSIe3LZdMfSU7ED2LEU=h900-rw"
-            width="100%"
-            alt="DashBoard Print"
-          >
-        </div>
-
-      </div>
-    </div>
-  </main>
-
-  <footer style="clear:both; background: #fff; padding: 10px 0; text-align: center;">
-    <div style="max-width: 768px; margin: 0 auto; padding: 0 10px;">
-      <span style="font-size: 12px">
-        Não quer receber mais e-mails como esse? <a href="#">Desinscreva-se aqui.</a>
-      </span>
-    </div>
-  </footer>
+    </footer>
 
 </body>
+
 </html>
