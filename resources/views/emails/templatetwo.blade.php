5274ecaafedbe72e4ec9ad897743bf58739de652
Andre <andrebf4@gmail.com>
2017-07-18 16:37:49 -0300

    improvements templatetwo

--- resources/views/emails/templatetwo.blade.php
+++ resources/views/emails/templatetwo.blade.php
@@ -1,5 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" style="height:100%; margin:0 auto; padding:0; width:100%" height="100%"
       width="100%">
 <head>
@@ -15,7 +14,7 @@
    <span class="hover-edit-container">
    <i class="zmdi zmdi-edit"></i>
    </span>
-    <table width="100%" border="0" cellspacing="0" cellpadding="30" background=""
+    <table width="100%" border="0" cellspacing="0" cellpadding="30"
            style="background-color: rgb(241, 243, 244); color: rgb(85, 85, 85);">
         <tbody>
         <tr>
@@ -29,7 +28,7 @@
 
                         <!-- HEADER LOGO WE PLANNER -->
                         <tr>
-                            <td style="padding: 0; text-align: center; max-width: 80% !important; background-color: rgb(255, 255, 255); color: rgb(85, 85, 85); "
+                            <td style="padding: 0; text-align: center; max-width: 100% !important; background-color: rgb(123, 204, 198); color: rgb(85, 85, 85); "
                                 align="center">
                                 <img
                                     width="100%"
@@ -39,15 +38,16 @@
                             </td>
                         </tr>
                         <tr>
-                            <td style="padding: 0; text-align: center; max-width: 80% !important; background-color: #75CDC7; color: rgb(85, 85, 85); "
+                            <td style="padding: 0; text-align: center; max-width: 100% !important; background-color: #75CDC7; color: rgb(242, 242, 242); "
                                 align="center">
-                                <h3>Teste</h3>
+                                <h2>Nome Do Local</h2>
+                                <span>18.07.2017</span>
                             </td>
 
                         </tr>
                         <tr>
 
-                            <td style="padding: 0; text-align: center; max-width: 80% !important; background-color: rgb(255, 255, 255); color: rgb(85, 85, 85); "
+                            <td style="padding: 0; text-align: center; max-width: 100% !important; background-color: rgb(255, 255, 255); color: rgb(85, 85, 85); "
                                 align="center">
                                 <img
                                     width="100%"
@@ -56,129 +56,107 @@
                                 />
                             </td>
                         </tr>
-
-                        <tr>
-
-                            <td style="padding: 0; text-align: center; max-width: 80% !important; background-color: rgb(255, 255, 255); color: rgb(85, 85, 85); "
-                                align="center">
-                                <h2>Texto</h2>
-                            </td>
-                        </tr>
-
-                        <?php
-                            if(!isset($data['align']) && empty($data['align'])){
-                                $data['align'] = 'center';
-                            }
-                        ?>
-
-                        <!-- TITULO -->
-                        @if(isset($data['messageTitle']) && !empty($data['messageTitle']))
-                        <tr>
-                            <td style="padding: 10px 20px; text-align: {{ $data['align'] }}; max-width: 80% !important; background-color: rgb(255, 255, 255); color: rgb(85, 85, 85);"
-                                align="center">
-                                <span style="font-size: 16px;">
-                                    {!! $data['messageTitle'] !!}
-                                </span>
-                            </td>
-                        </tr>
-                        @endif
-
-                        <!-- MESSAGE TWO -->
-                        @if(isset($data['messageOne']) && !empty($data['messageOne']))
-                        <tr>
-                            <td style="padding: 10px 20px; text-align: {{ $data['align'] }}; max-width: 80% !important; background-color: rgb(255, 255, 255); color: rgb(85, 85, 85);"
-                                align="{{ $data['align'] }}">
-                                <p style="text-align: {{ $data['align'] }}; ">
-                                    <span style="font-size: 14px;">
-                                        {!! $data['messageOne'] !!}
-                                    </span>
-                                </p>
-                            </td>
-                        </tr>
-                        @endif
-
-                        <!-- MESSAGE THREE -->
-                        @if(isset($data['messageTwo']) && !empty($data['messageTwo']))
-                        <tr>
-                            <td style="padding: 10px 20px; text-align: {{ $data['align'] }}; max-width: 80% !important; background-color: rgb(255, 255, 255); color: rgb(85, 85, 85);"
-                                align="{{ $data['align'] }}">
-                                <p style="text-align: {{ $data['align'] }}; ">
-                                    <span style="font-size: 14px;">
-                                        {!! $data['messageTwo'] !!}
-                                    </span>
-                                </p>
-                            </td>
-                        </tr>
-                        @endif
-
-                        <!-- MESSAGE THREE -->
-                        @if(isset($data['messageThree']) && !empty($data['messageThree']))
-                        <tr>
-                            <td style="padding: 10px 20px; text-align: {{ $data['align'] }}; max-width: 80% !important; background-color: rgb(255, 255, 255); color: rgb(85, 85, 85);"
-                                align="{{ $data['align'] }}">
-                                <p style="text-align: {{ $data['align'] }}; ">
-                                    <span style="font-size: 14px;">
-                                        {!! $data['messageThree'] !!}
-                                    </span>
-                                </p>
-                            </td>
-                        </tr>
-                        @endif
-
-                        <!-- MESSAGE THREE -->
-                        @if(isset($data['access_code']) && !empty($data['access_code']))
-                        <tr>
-                            <td style="padding: 10px 20px; text-align: {{ $data['align'] }}; max-width: 80% !important; background-color: rgb(255, 255, 255); color: rgb(85, 85, 85);"
-                                align="{{ $data['align'] }}">
-                                <p style="text-align: {{ $data['align'] }}; ">
-                                    <span style="font-size: 22px;">
-                                        {!!$data['access_code'] !!}
-                                    </span>
-                                </p>
-                            </td>
-                        </tr>
-                        @endif
-                        
-                        <!-- BOTÃO -->
-
-                        @if(isset($data['button_link']) && !empty($data['button_link']))
-                        <tr>
-                            <td style="padding: 20px; text-align: center; max-width: 80% !important; background-color: rgb(255, 255, 255);"
-                                align="center">
-                                <center>
-                                    <a style="transition: all 100ms ease-in; display: block; font-weight: bold; text-align: center; margin: 10px 10px 10px; text-decoration: none; max-width: 80% !important; background-color: rgb(255, 255, 255);"
-                                       class="button-a" align="center"
-                                       href="{{$data['button_link']}}">
-                                        <span style="color: rgb(255, 255, 255); border-color: #69A7BE; background-color: #69A7BE; width: 200px; height: 70px; border-radius: 5px; border-width: 5px; font-size: 20px; padding: 15px 30px 15px 30px; margin: 30px 10px;">
-                                            {{{$data['button_name']}}}
-                                        </span>
-                                    </a>
-                                </center>
-                            </td>
-                        </tr>
-                        @endif
-
-                        <!-- ULTIMO BLOCO -->
-                        @if(isset($data['messageFour']) && !empty($data['messageFour']))
-                        <tr>
-                            <td style="padding: 20px 20px 40px 20px; text-align: {{ $data['align'] }}; max-width: 80% !important; background-color: rgb(255, 255, 255); color: rgb(85, 85, 85);"
-                                align="{{ $data['align'] }}">
-                                <p style="text-align: {{ $data['align'] }}; "><span style="font-size: 14px;">
-                                    {!! $data['messageFour'] !!}
-                                </span>
-                                </p>
-
-                            </td>
-                        </tr>
-                        @endif
                         </tbody>
                     </table>
+
+                    <!-- MAIN CONTENT   -->
+                    <table
+                        cellspacing="0"
+                        cellpadding="0"
+                        align="center"
+                        width="440px"
+                        role="presentation"
+                        style="background-color: #fff; padding: 30px 0;"
+                    >
+                        <tbody style="text-align: center; font-family: 'Lato', sans-serif;">
+                            <tr style="width: 100%;">
+                                <td style="padding: 15px 0; border-right: 1px solid rgb(242, 242, 242); width: 50%;">
+                                    <h3 style="font-family: 'Quicksand', sans-serif; margin: 0 !important;">Visualizações</h3>
+                                    <span style="font-size: 25px;">123</span>
+                                    <br />
+                                    <small>Total: <strong>12345</strong></small>
+                                </td>
+                                <td style="padding: 15px 0; width: 50%;">
+                                    <h3 style="font-family: 'Quicksand', sans-serif; margin: 0 !important;">Tempo de Visualização</h3>
+                                    <span style="font-size: 25px;">123/<small>horas</small></span>
+                                    <br />
+                                    <small>Total: <strong>12345/horas</strong></small>
+                                </td>
+                            </tr>
+                            <tr style="width: 100%;">
+                                <td style="padding: 15px 0; border-right: 1px solid rgb(242, 242, 242); border-top: 1px solid rgb(242, 242, 242); width: 50%;">
+                                    <h3 style="font-family: 'Quicksand', sans-serif; margin: 0 !important;">Contato <small>(whatsapp)</small></h3>
+                                    <span style="font-size: 25px;">123</span>
+                                    <br />
+                                    <small>Total: <strong>12345</strong></small>
+                                </td>
+                                <td style="padding: 15px 0; border-top: 1px solid rgb(242, 242, 242); width: 50%;">
+                                    <h3 style="font-family: 'Quicksand', sans-serif; margin: 0 !important;">Contato <small>(Mensagem)</small></h3>
+                                    <span style="font-size: 25px;">123</span>
+                                    <br />
+                                    <small>Total: <strong>12345</strong></small>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <!-- /MAIN CONTENT -->
+
+                    <table
+                        cellspacing="0"
+                        cellpadding="0"
+                        align="center"
+                        width="440px"
+                        role="presentation"
+                        style="background-color: #fff; padding: 30px;"
+                    >
+                        <tbody style="font-family: 'Lato', sans-serif;">
+                            <tr>
+                                <td style="padding: 5px 0; background-color: rgb(255, 255, 255); width: 70%;">
+                                    <h2 style="font-size: 20px; text-transform: uppercase; font-family: 'Quicksand', sans-serif; margin: 0 !important;">Compartilhamentos</h2>
+                                </td>
+                                <td style="background-color: rgb(255, 255, 255); width: 30%;">
+                                </td>
+                            </tr>
+                            <tr style="width: 100%">
+                                <td style="padding: 15px; border-radius:4px 0px 0px 4px; background-color: rgb(242, 242, 242); width: 70%; border: 1px solid rgb(255, 255, 255)">
+                                    <h3 style="font-family: 'Quicksand', sans-serif; margin: 0 !important; color: #3b5998; ">Facebook</h3>
+                                </td>
+                                <td style="padding: 15px; border-radius:0px 4px 4px 0px; background-color: rgb(242, 242, 242); border-left: 1px solid rgb(255, 255, 255); border-top: 1px solid rgb(255, 255, 255); width: 30%;">
+                                    <span><strong>123</strong></span>
+                                    <br />
+                                    <small><strong style="">Total: 12345</strong></small>
+                                </td>
+                            </tr>
+                            <tr style="width: 100%">
+                                <td style="padding: 15px; border-radius:4px 0px 0px 4px; background-color: rgb(242, 242, 242); width: 70%; border: 1px solid rgb(255, 255, 255)">
+                                    <h3 style="font-family: 'Quicksand', sans-serif; margin: 0 !important; color: #009688; ">Whatsapp</h3>
+                                </td>
+                                <td style="padding: 15px; border-radius:0px 4px 4px 0px; background-color: rgb(242, 242, 242); border-left: 1px solid rgb(255, 255, 255); border-top: 1px solid rgb(255, 255, 255); width: 30%;">
+                                    <span><strong>123</strong></span>
+                                    <br />
+                                    <small><strong style="">Total: 12345</strong></small>
+                                </td>
+                            </tr>
+                            <tr style="width: 100%">
+                                <td style="padding: 15px; border-radius:4px 0px 0px 4px; background-color: rgb(242, 242, 242); width: 70%; border: 1px solid rgb(255, 255, 255)">
+                                    <h3 style="font-family: 'Quicksand', sans-serif; margin: 0 !important; color: #999;">Link copiados</h3>
+                                </td>
+                                <td style="padding: 15px; border-radius:0px 4px 4px 0px; background-color: rgb(242, 242, 242); border-left: 1px solid rgb(255, 255, 255); border-top: 1px solid rgb(255, 255, 255); width: 30%;">
+                                    <span><strong>123</strong></span>
+                                    <br />
+                                    <small><strong style="">Total: 12345</strong></small>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+
                     <!-- INIT FOOTER WEPLANNER -->
-                    <table 
-                        cellspacing="0" 
-                        cellpadding="0" 
-                        border="0" 
-                        align="center" 
+                    <table
+                        cellspacing="0"
+                        cellpadding="0"
+                        border="0"
+                        align="center"
                         width="440px"
                         style="mso-table-lspace:0; mso-table-rspace:0; border-collapse:collapse; border-spacing:0; margin:0 auto; table-layout:fixed; max-width:100%; background-color: #EDECEC"
                         role="presentation">
