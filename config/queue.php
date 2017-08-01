337945b6a9e72f3e89f462dacdf402fd2f90e14f
Matheus Lima <contato@matheuslima.com.br>
2017-08-01 14:32:23 -0400

    Chnage queue config

--- config/queue.php
+++ config/queue.php
@@ -44,7 +44,7 @@ return [
         'beanstalkd' => [
             'driver' => 'beanstalkd',
             'host' => 'localhost',
-            'queue' => 'default',
+            'queue' => env('QUEUE_NAME', 'default'),
             'retry_after' => 90,
         ],
 
