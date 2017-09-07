a102d09d9ee15abfe3fc125aa3f3949156dac97a
Igor Trindade <igorlucast@hotmail.com>
2017-09-07 18:22:31 -0300

    Minroc hanges

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -5,8 +5,6 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta http-equiv="cache-control" content="public">
   <meta http-equiv="expires" content ="<?php echo date('l jS \of F Y h:i:s A', strtotime('+7 days')); ?>" />
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
 
   @include('components.seo-opengraph')
 
