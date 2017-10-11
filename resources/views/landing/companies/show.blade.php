870bd50cfb9dc542aff620a8b56202082cff7f79
Andre Brandao <andrebf4@gmail.com>
2017-10-11 16:42:35 -0300

    added functions to open share in place show

--- resources/views/landing/companies/show.blade.php
+++ resources/views/landing/companies/show.blade.php
@@ -133,10 +133,10 @@
                             @endif
                             <!-- Website -->
 
-                            <button type="button" class="btn btn-xs btn-block btn-facebook m-t-20 p-5 f-15" @click="openShareFacebook()">
+                            <button type="button" class="btn btn-xs btn-block btn-facebook open-share-facebook m-t-20 p-5 f-15">
                                 <i class="ion-social-facebook m-r-5" ></i>Compartilhar no facebook
                             </button>
-                            <button type="button" class="btn btn-xs btn-block btn-whatsapp m-t-5 p-5 f-15" @click="openShareWhatsapp()">
+                            <button type="button" class="btn btn-xs btn-block btn-whatsapp open-share-whatsapp m-t-5 p-5 f-15">
                                 <i class="ion-social-whatsapp m-r-5"></i>Compartilhar no whatsapp
                             </button>
 
@@ -318,7 +318,7 @@
                 @endif
                 <!-- / Decorations -->
 
-                <!-- Anuncios 
+                <!-- Anuncios
                 <div class="col-sm-12">
                     <div class="card">
                         <div class="card-header ch-alt text-center">
@@ -342,6 +342,16 @@
             var target = e.target.dataset.target
             $(e.target).addClass('out')
             $(target).addClass('in')
+        })
+
+        $('.open-share-facebook').on('click', function() {
+            var url = `https://www.facebook.com/dialog/share?app_id=151705885358217&href=https://weplaces.com.br/espacos/{{ $place->slug }}&display=popup&mobile_iframe=true`;
+            window.open(url, '_blank', 'location=yes');
+        })
+        $('.open-share-whatsapp').on('click', function() {
+            // Whatsapp share
+            var url = `https://api.whatsapp.com/send?text=Confira o espaço {{ $place->name }} no weplaces, veja o abaixo: https://weplaces.com.br/espacos/{{ $place->slug }}`;
+            window.open(url, '_system', null);
         })
 
         var coverSwiper = new Swiper($('.place-photos'), {
